56d8fc79df00167a3f7882a9aeac9a5a6a2afb7d
René Zurita Corro <zc.rene@gmail.com>
2022-10-28 00:48:19 -0500

    update: added tests and fix errors

--- src/datacapture/__init__.py
+++ src/datacapture/__init__.py
@@ -7,6 +7,7 @@ class Stats:
         "Stats constructor"
         self.counter = Counter(sorted_data)
         self.keys = list(self.counter)
+        print(f'sorted_data: {sorted_data}')
         print(f'keys: {self.keys}')
         print(f'counter: {self.counter}')
         acc = 0
@@ -19,19 +20,15 @@ class Stats:
     def less(self, value):
         "Return number of elements less than value"
         idx = bisect_left(self.keys, value)
-        print(f'idx: {idx}')
-        print(f'key: {self.keys[idx]}')
         return self.counter[self.keys[idx - 1]] if idx > 0 else 0
 
     def greater(self, value):
         "Return number of elements greater than value"
         idx = bisect_right(self.keys, value)
-        print(f'idx: {idx}')
-        print(f'key: {self.keys[idx]}')
-        return self.total - self.counter[self.keys[idx - 1]] if idx > 0 else 0
+        return self.total - self.counter[self.keys[idx - 1]] if idx > 0 else self.total
 
     def between(self, low, upper):
-        return self.less(upper + 1) - self.greater(low - 1)
+        return self.total - self.greater(upper) - self.less(low)
 
 
 class DataCapture:
--- tests/test_datacapture.py
+++ tests/test_datacapture.py
@@ -22,14 +22,30 @@ def stats(capture):
 
 def test_less(stats):
     "should return 2 (only two values 3, 3 are less than 4)"
+    assert stats.less(0) == 0
+    assert stats.less(3) == 0
     assert stats.less(4) == 2
+    assert stats.less(3) == 0
+    assert stats.less(6) == 3
+    assert stats.less(10) == 5
 
 
 def test_greater(stats):
     "should return 2 (6 and 9 are the only two values greater than 4)"
+    assert stats.greater(0) == 5
     assert stats.greater(4) == 2
+    assert stats.greater(5) == 2
+    assert stats.greater(2) == 5
+    assert stats.greater(3) == 3
+    assert stats.greater(6) == 1
+    assert stats.greater(7) == 1
+    assert stats.greater(9) == 0
+    assert stats.greater(10) == 0
 
 
 def test_between(stats):
     "should return 4 (3, 3, 4 and 6 are between 3 and 6)"
     assert stats.between(3, 6) == 4
+    assert stats.between(3, 9) == 5
+    assert stats.between(2, 10) == 5
+    assert stats.between(4, 9) == 3
