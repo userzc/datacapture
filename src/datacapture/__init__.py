286d8c17bc994cd0d0c0f5087489ee95a46477cc
René Zurita Corro <zc.rene@gmail.com>
2022-10-28 11:53:54 -0500

    update: time complexity requirement compliance

--- src/datacapture/__init__.py
+++ src/datacapture/__init__.py
@@ -1,13 +1,82 @@
-from bisect import (insort, bisect_left, bisect_right)
+from bisect import (bisect_left, bisect_right)
 from collections import Counter
 
 
 class Stats:
-    def __init__(self, sorted_data):
-        "Stats constructor"
-        self.counter = Counter(sorted_data)
+    def __init__(self, data):
+        "Stats constructor, O(n) time complexity"
+        data.sort()
+        self.counter = Counter(data)
         self.keys = list(self.counter)
-        print(f'sorted_data: {sorted_data}')
+        self.keys.sort()
+        self.inv_keys = {}
+        print(f'data: {data}')
+        print(f'keys: {self.keys}')
+        print(f'counter: {self.counter}')
+        acc = 0
+        for idx, key in enumerate(self.keys):
+            self.counter[key] += acc
+            acc = self.counter[key]
+            self.inv_keys[key] = idx
+        self.total = acc
+        print(f'inv_keys: {self.inv_keys}')
+        print(f'updated counter: {self.counter}')
+
+    def less(self, value):
+        """Return number of elements less than value, O(1) time complexity,
+         only for values already on data
+
+        """
+        idx = self.inv_keys[value]
+        return self.counter[self.keys[idx - 1]] if idx > 0 else 0
+
+    def greater(self, value):
+        """Return number of elements greater than value, O(1) time complexity,
+         only for values already on data
+
+        """
+        idx = self.inv_keys[value]
+        idx += 1
+        return self.total - self.counter[self.keys[idx - 1]] if idx > 0 else self.total
+
+    def between(self, low, upper):
+        """Return number of elements in the [low, upper] range, O(1) time
+         complexity, only for values already on data
+
+        """
+        return self.total - self.greater(upper) - self.less(low)
+
+
+class DataCapture:
+    def __init__(self):
+        "DataCapture constructor"
+        self.data_elems = []
+
+    def add(self, elem):
+        "Add elements, O(1) time complexity"
+        self.data_elems.append(elem)
+
+    def build_stats(self):
+        """Build stats of captured data, O(n) time complexity, only for values
+        already in data
+
+        """
+        return Stats(self.data_elems)
+
+    def build_stats_b(self):
+        """Build stats of captured data, O(n) time complexity, useful for any values
+
+        """
+        return StatsB(self.data_elems)
+
+
+class StatsB:
+    def __init__(self, data):
+        "Stats constructor, O(n) time complexity, relies on bisectiion search"
+        data.sort()
+        self.counter = Counter(data)
+        self.keys = list(self.counter)
+        print(f'data: {data}')
         print(f'keys: {self.keys}')
         print(f'counter: {self.counter}')
         acc = 0
@@ -18,27 +87,24 @@ class Stats:
         print(f'updated counter: {self.counter}')
 
     def less(self, value):
-        "Return number of elements less than value"
+        """Return number of elements less than value, O(n * log(n)) time
+         complexity, for any int values
+
+        """
         idx = bisect_left(self.keys, value)
         return self.counter[self.keys[idx - 1]] if idx > 0 else 0
 
     def greater(self, value):
-        "Return number of elements greater than value"
+        """Return number of elements greater than value, O(n * log(n)) time
+         complexity, for any int values
+
+        """
         idx = bisect_right(self.keys, value)
         return self.total - self.counter[self.keys[idx - 1]] if idx > 0 else self.total
 
     def between(self, low, upper):
+        """Return number of elements in the range [low, upper], O(n * log(n)) time
+         complexity, for any int values
+
+        """
         return self.total - self.greater(upper) - self.less(low)
-
-
-class DataCapture:
-    def __init__(self):
-        "DataCapture constructor"
-        self.data_elems = []
-
-    def add(self, elem):
-        insort(self.data_elems, elem)
-
-    def build_stats(self):
-        "Build stats of captured data"
-        return Stats(self.data_elems)
--- tests/test_datacapture.py
+++ tests/test_datacapture.py
@@ -15,37 +15,66 @@ def capture():
 
 
 @pytest.fixture
+def stats_b(capture):
+    "Instantiate Stats class from filled DataCapture class"
+    return capture.build_stats_b()
+
+@pytest.fixture
 def stats(capture):
     "Instantiate Stats class from filled DataCapture class"
     return capture.build_stats()
 
 
+def test_less_b(stats_b):
+    "should return 2 (only two values 3, 3 are less than 4)"
+    assert stats_b.less(0) == 0
+    assert stats_b.less(3) == 0
+    assert stats_b.less(4) == 2
+    assert stats_b.less(3) == 0
+    assert stats_b.less(6) == 3
+    assert stats_b.less(10) == 5
+
+
+def test_greater_b(stats_b):
+    "should return 2 (6 and 9 are the only two values greater than 4)"
+    assert stats_b.greater(0) == 5
+    assert stats_b.greater(4) == 2
+    assert stats_b.greater(5) == 2
+    assert stats_b.greater(2) == 5
+    assert stats_b.greater(3) == 3
+    assert stats_b.greater(6) == 1
+    assert stats_b.greater(7) == 1
+    assert stats_b.greater(9) == 0
+    assert stats_b.greater(10) == 0
+
+
+def test_between_b(stats_b):
+    "should return 4 (3, 3, 4 and 6 are between 3 and 6)"
+    assert stats_b.between(3, 6) == 4
+    assert stats_b.between(3, 9) == 5
+    assert stats_b.between(2, 10) == 5
+    assert stats_b.between(4, 9) == 3
+
+
 def test_less(stats):
     "should return 2 (only two values 3, 3 are less than 4)"
-    assert stats.less(0) == 0
     assert stats.less(3) == 0
     assert stats.less(4) == 2
     assert stats.less(3) == 0
     assert stats.less(6) == 3
-    assert stats.less(10) == 5
 
 
 def test_greater(stats):
     "should return 2 (6 and 9 are the only two values greater than 4)"
-    assert stats.greater(0) == 5
     assert stats.greater(4) == 2
-    assert stats.greater(5) == 2
-    assert stats.greater(2) == 5
     assert stats.greater(3) == 3
     assert stats.greater(6) == 1
-    assert stats.greater(7) == 1
     assert stats.greater(9) == 0
-    assert stats.greater(10) == 0
 
 
-def test_between(stats):
+def testetween(stats):
     "should return 4 (3, 3, 4 and 6 are between 3 and 6)"
     assert stats.between(3, 6) == 4
     assert stats.between(3, 9) == 5
-    assert stats.between(2, 10) == 5
     assert stats.between(4, 9) == 3
+    assert stats.between(3, 9) == 5
