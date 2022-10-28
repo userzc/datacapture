6939e0f3410451dc2460e95bb4f047f5c1c29ed7
René Zurita Corro <zc.rene@gmail.com>
2022-10-28 00:18:49 -0500

    update: test fixtures

--- tests/test_datacapture.py
+++ tests/test_datacapture.py
@@ -1,14 +1,35 @@
+import pytest
 from datacapture import DataCapture
 
 
-def test_add():
+@pytest.fixture
+def capture():
+    "Instantiate DataCapture class"
     capture = DataCapture()
     capture.add(3)
     capture.add(9)
     capture.add(3)
     capture.add(4)
     capture.add(6)
-    stats = capture.build_stats()
-    assert stats.less(4) == 2 # should return 2 (only two values 3, 3 are less than 4)
-    assert stats.between(3, 6) == 4 # should return 4 (3, 3, 4 and 6 are between 3 and 6)
-    assert stats.greater(4) == 2 # should return 2 (6 and 9 are the only two values greater than 4)
+    return capture
+
+
+@pytest.fixture
+def stats(capture):
+    "Instantiate Stats class from filled DataCapture class"
+    return capture.build_stats()
+
+
+def test_less(stats):
+    "should return 2 (only two values 3, 3 are less than 4)"
+    assert stats.less(4) == 2
+
+
+def test_greater(stats):
+    "should return 2 (6 and 9 are the only two values greater than 4)"
+    assert stats.greater(4) == 2
+
+
+def test_between(stats):
+    "should return 4 (3, 3, 4 and 6 are between 3 and 6)"
+    assert stats.between(3, 6) == 4
